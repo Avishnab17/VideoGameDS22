1645977194a80900a5cc03854e567ec8456b49df
yashi <yashinayurvi@gmail.com>
2022-06-09 14:35:59 +0400

    testing suceeded

--- Players.java
+++ Players.java
@@ -1,5 +1,8 @@
 public class Players {
 
+    public static void main(String[] args){
+        System.out.println("COOL");
+    }
     // Hi I have tried to create a Players.java
     //clone successful // it's *succ* not *suc* lady // oh god :)
     //dear god
